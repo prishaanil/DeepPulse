a2df2f4a297b501422ae045a666a046901031d8d
paz3b <paz3b@rs348cz44.managed.mst.edu>
2025-04-18 13:11:44 -0500

    pls work

--- datautils.py
+++ datautils.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 import pandas as pd
 import torch
@@ -7,15 +9,173 @@ import sys
 from src.data.datamodule import DataLoaders
 from src.data.pred_dataset import *
 
-DSETS = ['PTB-XL']
+DSETS = ['ettm1', 'ettm2', 'etth1', 'etth2', 'electricity',
+         'traffic', 'illness', 'weather', 'exchange', 'PTB-XL',
+        ]
 
 def get_dls(params):
     
     assert params.dset in DSETS, f"Unrecognized dset (`{params.dset}`). Options include: {DSETS}"
     if not hasattr(params,'use_time_features'): params.use_time_features = False
 
-    if params.dset == 'PTB-XL':
-        root_path = 'src/data/datasets/physionet.org/files/ptb-xl/1.0.3'
+    if params.dset == 'ettm1':
+        root_path = '/data/datasets/public/ETDataset/ETT-small/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_ETT_minute,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'ETTm1.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+
+
+    elif params.dset == 'ettm2':
+        root_path = '/data/datasets/public/ETDataset/ETT-small/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_ETT_minute,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'ETTm2.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+
+    elif params.dset == 'etth1':
+        root_path = '/data/datasets/public/ETDataset/ETT-small/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_ETT_hour,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'ETTh1.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+
+
+    elif params.dset == 'etth2':
+        root_path = '/data/datasets/public/ETDataset/ETT-small/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_ETT_hour,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'ETTh2.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+    
+
+    elif params.dset == 'electricity':
+        root_path = '/data/datasets/public/electricity/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_Custom,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'electricity.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+
+    elif params.dset == 'traffic':
+        root_path = '/data/datasets/public/traffic/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_Custom,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'traffic.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+    
+    elif params.dset == 'weather':
+        root_path = '/data/datasets/public/weather/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_Custom,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'weather.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+
+    elif params.dset == 'illness':
+        root_path = '/data/datasets/public/illness/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_Custom,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'national_illness.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+
+    elif params.dset == 'exchange':
+        root_path = '/data/datasets/public/exchange_rate/'
+        size = [params.context_points, 0, params.target_points]
+        dls = DataLoaders(
+                datasetCls=Dataset_Custom,
+                dataset_kwargs={
+                'root_path': root_path,
+                'data_path': 'exchange_rate.csv',
+                'features': params.features,
+                'scale': True,
+                'size': size,
+                'use_time_features': params.use_time_features
+                },
+                batch_size=params.batch_size,
+                workers=params.num_workers,
+                )
+        
+    elif params.dset == 'PTB-XL':
+        root_path = 'src/data/datasets/physionet.org/files/ptb-xl/1.0.3/'
         size = [params.context_points, 0, params.target_points]
         dls = DataLoaders(
                 datasetCls=Dataset_Custom,
@@ -34,6 +194,9 @@ def get_dls(params):
     dls.vars, dls.len = dls.train.dataset[0][0].shape[1], params.context_points
     dls.c = dls.train.dataset[0][1].shape[0]
     return dls
+
+
+    # dataset is assume to have dimension len x nvars
 
 
 
